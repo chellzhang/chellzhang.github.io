260ed3f6da1ed050d7aab0d56840e590ba37a6bb
ChellZhang <chell.czhang@gmail.com>
2023-02-07 23:45:21 -0700

    added scrollreveal for page 1

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
-    $('#fullpage').fullpage();
+  $('#fullpage').fullpage();
+  ScrollReveal().reveal('#greeting-box h1',{ delay: 300, origin:'bottom', distance: '10px', interval: 300, duration: 1500 });
+  ScrollReveal().reveal('.linkbox a', {delay:1500, origin:'right', distance: '10px',interval: 500})
 });
 
 var swiper = new Swiper(".slide-container", {
-    slidesPerView: 5,
-    initialSlide: 1,
-    spaceBetween: 0,
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
-  });
+  slidesPerView: 5,
+  initialSlide: 1,
+  spaceBetween: 0,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+
+
