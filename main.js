1613fe5b328ac6155ba28e02d0ae871b82828edf
ChellZhang <chell.czhang@gmail.com>
2023-02-09 12:41:42 -0700

    emailjs function done. works

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    licenseKey: 'gplv3-license'
+  });
   ScrollReveal().reveal('#greeting-box h1',{ delay: 300, origin:'bottom', distance: '10px', interval: 300, duration: 1500 });
   ScrollReveal().reveal('.linkbox a', {delay:1500, origin:'right', distance: '10px',interval: 500})
+});
+
+(function() {
+  emailjs.init('rIOs5_3AhoxfpSpmS');
+})()
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  this.contact_number.value = Math.random() * 100000 | 0;
+  emailjs.sendForm('service_bbp52rj', 'contact_form', this)
+      .then(function() {
+          console.log('SUCCESS!');
+      }, function(error) {
+          console.log('FAILED...', error);
+      });
 });
 
 var swiper = new Swiper(".slide-container", {
@@ -14,5 +31,3 @@ var swiper = new Swiper(".slide-container", {
     clickable: true
   }
 });
-
-
