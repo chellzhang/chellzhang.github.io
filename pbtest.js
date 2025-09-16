cc18a09168f2e94443c1677c9f1689a1de0a0275
Chell Zhang <116301702+chellzhang@users.noreply.github.com>
2025-09-16 10:44:53 -0600

    Update pbtest.js

--- pbtest.js
+++ pbtest.js
@@ -1,6 +1,6 @@
 {
   "PB coaching":"Cloud Control- Take the Reins with OneDrive",
-  "PB coaching link":"http://www.google.com",
+  "PB coaching link":"https://copilot.microsoft.com/",
   "TotW":"Insert Data from Pictures",
   "TotW link":"https://app.brainstorminc.com/discover/flows/471253"
 }
